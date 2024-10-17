093803f18ec65a2e514975ee9b6d1dcf1c891e24
Rafael Silva Willians <rafael.willians@outlook.com>
2024-10-17 01:15:58 +0000

    Adicionado script login aws

---
+++ aws-cli-login.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Solicita as credenciais (Session Token é opcional)
+read -p "AWS Access Key ID: " AWS_ACCESS_KEY_ID
+read -p "AWS Secret Access Key: " AWS_SECRET_ACCESS_KEY
+read -p "AWS Session Token: " AWS_SESSION_TOKEN
+read -p "AWS Default Region: " AWS_DEFAULT_REGION
+
+# Sobrescreve as variáveis de ambiente
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
+export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+
+# Salva (sobrescreve) o arquivo ~/.aws/credentials
+mkdir -p ~/.aws
+cat <<EOL > ~/.aws/credentials
+[default]
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+aws_session_token = $AWS_SESSION_TOKEN
+EOL
+
+# Sobrescrever o arquivo ~/.aws/config com a região padrão
+cat <<EOL > ~/.aws/config
+[default]
+region = $AWS_DEFAULT_REGION
+EOL
+
+echo -e "Env Variables for AWS credentials overwritten!\nAWS Credentials ready!"
