043b3f21b37c9ecafd1fd7197aeb33be7f6cd344
Rafael Silva Willians <rafael.willians@outlook.com>
2024-10-21 19:27:49 +0000

    Teste script novo e retirada variavees ambiente

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Regiao padrao
+read -p "AWS Default Region: " AWS_DEFAULT_REGION
+
+# Informa o usuário para colar o texto
+echo "Cole o texto que deseja salvar como credenciais da AWS. Pressione CTRL+D para finalizar a entrada."
+
+# Lê a entrada do usuário e salva na variável AWS_CREDENTIALS
+AWS_CREDENTIALS=$(cat)
+
+# Salva o conteúdo da variável no arquivo de destino
+mkdir -p ~/.aws
+cat <<EOL > ~/.aws/credentials
+$AWS_CREDENTIALS
+EOL
+
+cat <<EOL > ~/.aws/config
+[default]
+region = $AWS_DEFAULT_REGION
+EOL
+
+echo "As credenciais foram salvas."
